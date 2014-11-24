0170c5b97eb9ad5b3006f3bc7c5ab39b2291cf72
Mark <mvandoesum@hotmail.com>
2014-11-24 10:39:50 +0100

    renamed class

--- src/main/BayesianClassifier.java
+++ src/main/BayesianClassifier.java
@@ -1,12 +1,12 @@
 package main;
 
-public class Tokenizer {
+public class BayesianClassifier {
 	public String[] tokenizer(String input) {
 		return input.replaceAll("(?!\")\\p{Punct}", "").toLowerCase().split("\\s+");
 	}
 	
 	public static void main(String[] args) {
-		Tokenizer token = new Tokenizer();
+		BayesianClassifier token = new BayesianClassifier();
 		String str = "Hello, everyone. Do you like the new layout?";
 		String[] tokens = token.tokenizer(str);
 		for (int i = 0; i < tokens.length; i++) {
